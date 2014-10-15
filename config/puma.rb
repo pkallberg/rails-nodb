3709c6dbbc2b10369f617465121afb46a09a1da8
Philip Kallberg <philip@kallberg.me>
2014-10-15 11:44:03 +0100

    update

--- config/puma.rb
+++ config/puma.rb
@@ -2,9 +2,10 @@
 threads 1, 6
 workers 2
 
+preload_app!
+
 on_worker_boot do
-  require "active_record"
-  cwd = File.dirname(__FILE__)+"/.."
-  ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
-  ActiveRecord::Base.establish_connection(ENV["DATABASE_URL"] || YAML.load_file("#{cwd}/config/database.yml")[ENV["RAILS_ENV"]])
+  ActiveSupport.on_load(:active_record) do
+    ActiveRecord::Base.establish_connection
+  end
 end
